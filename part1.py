6e010caf789f9e74db24f28f39f9649d2fce59bd
MittBrukernavn <mittbrukernavn.aa@gmail.com>
2020-09-24 12:59:28 +0200

    Parsing and inserting data to the database

--- part1.py
+++ part1.py
@@ -16,15 +16,15 @@ def main():
             has_labels BOOLEAN NOT NULL
         ''',
         'Activity': '''
-            id INT PRIMARY KEY NOT NULL,
+            id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
             user_id CHAR(3) NOT NULL,
             transportation_mode ENUM('walk', 'bike', 'bus', 'car', 'subway', 'train', 'airplane', 'boat', 'run', 'motorcycle', 'taxi') DEFAULT NULL,
-            start_date_time TEXT NOT NULL,
-            end_date_time TEXT NOT NULL,
+            start_date_time DATETIME NOT NULL,
+            end_date_time DATETIME NOT NULL,
             FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE
         ''',
         'Trackpoint': '''
-            id INT PRIMARY KEY NOT NULL,
+            id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
             activity_id INT NOT NULL,
             lat DOUBLE NOT NULL,
             lon DOUBLE NOT NULL,
@@ -50,24 +50,65 @@ def main():
 
     with open(f'{working_directory}/dataset/labeled_ids.txt') as f:
         for line in f:
+            # make sure our logic works (would fail e.g. if some person IDs are missing)
             assert line.strip() == user_data[int(line)][0]
             user_data[int(line)][1] = True
     
-    print(*user_data, sep='\n')
-    
+    # print(*user_data, sep='\n')
+    print('Inserting users...')
     connection.cursor.executemany('INSERT INTO User (id, has_labels) VALUES (%s, %s)', user_data)
     connection.commit()
+    print(len(user_data), 'users inserted')
 
-    # Insert activities:
+    # read data about activities and trackpoints
     activity_data = []
     trackpoint_data = []
+    activity_id = 1 # manually setting the activity IDs
+    # this allows us to know the foreign keys without needing to insert activities one by one and checking the inserted IDs
     for [user_id, has_labels] in user_data:
-        pass
-
+        labels = {}
+        if has_labels:
+            with open(f'{data_directory}/{user_id}/labels.txt', 'r') as f:
+                f.readline() # skip header
+                for line in f:
+                    start_date, start_time, end_date, end_time, transport_mode = line.strip().split()
+                    labels[f'{start_date} {start_time}'] = (f'{end_date} {end_time}', transport_mode)
+        # each .plt file is a single activity
+        _, _, activity_filenames = next(walk(f'{data_directory}/{user_id}/Trajectory'))
+        for activity_filename in activity_filenames:
+            with open(f'{data_directory}/{user_id}/Trajectory/{activity_filename}') as activity_file:
+                # skip 6 first lines
+                for _ in range(6):
+                    activity_file.readline()
+                lines = activity_file.readlines() # Doing a full read of the file might be risky
+                # but I really don't hope or believe there is any one file too big to open in memory 
+                if len(lines) > 2500: # skip activities with more than 2500 trackpoints
+                    continue
+                _, _, _, _, _, start_date, start_time = lines[0].strip().split(',')
+                _, _, _, _, _, end_date, end_time = lines[-1].strip().split(',')
+                transportation_mode = None
+                if f'{start_date} {start_time}' in labels: # if data is labeled
+                    end_date_and_time, mode = labels[f'{start_date} {start_time}']
+                    assert end_date_and_time == f'{end_date} {end_time}' # Just making sure
+                    transportation_mode = mode
+                activity_data.append([activity_id, user_id, transportation_mode, f'{start_date} {start_time}', f'{end_date} {end_time}'])
+                activity_id += 1
+                # activity data is dealt with - time to get trackpoint data
+                for line in lines:
+                    lat, lon, _, alt, date_days, date, time = lines[0].strip().split(',')
+                    trackpoint_data.append([activity_id, lat, lon, alt, date_days, f'{date} {time}'])
+    # Insert activities:
+    print('Inserting activities...')
+    connection.cursor.executemany('INSERT INTO Activity (id, user_id, transportation_mode, start_date_time, end_date_time) VALUES (%s, %s, %s, %s, %s)', activity_data)
+    connection.commit()
+    print(len(activity_data), 'activities inserted')
 
     # Insert trackpoints:
-
+    print('Inserting trackpoints...')
+    connection.cursor.executemany('INSERT INTO Trackpoint (activity_id, lat, lon, altitude, date_days, date_time) VALUES (%s, %s, %s, %s, %s, %s)', trackpoint_data)
+    connection.commit()
     connection.close_connection()
+    print(len(trackpoint_data), 'trackpoints inserted')
 
 if __name__ == '__main__':
     main()
